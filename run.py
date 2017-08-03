7fd0337d1b02d142ab7697274ddf3abda22ead99
SummerCheng <546589068wry@gmail.com>
2017-08-04 00:12:33 +0800

    我觉得不公平= =

--- run.py
+++ run.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 def homepage():
 	return render_template('index.html')
 
-@app.route('/<string:student_number>')
+@app.route('/<string:student_number>/details')
 def intro(student_number):
 	return render_template(str(student_number)+".html")
 
-@app.route('/A10134')
-def introl():
-	return render_template('html-test.html')
+# @app.route('/A10134')
+# def introl():
+# 	return render_template('html-test.html')
 
-@app.route('/A10509')
-def introv():
-	return render_template('v.html')
+# @app.route('/A10509')
+# def introv():
+# 	return render_template('v.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
