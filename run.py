45749aa1e7b327e082d91f3a2151c07dc68b4198
hydewww <2208493266@qq.com>
2017-08-03 15:53:02 +0800

    update route

--- run.py
+++ run.py
@@ -5,26 +5,20 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-	print ("h")
+def homepage():
 	return render_template('index.html')
+
+@app.route('/<string:student_number>')
+def intro(student_number):
+	return render_template(str(student_number)+".html")
 
 @app.route('/A10134')
 def introl():
 	return render_template('html-test.html')
 
-@app.route('/A14075')
-def intros():
-	return render_template('A14075.html')
-
-@app.route('/B20837')
-def introx():
-	return render_template('B20837xz.html')
-
 @app.route('/A10509')
 def introv():
 	return render_template('v.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
