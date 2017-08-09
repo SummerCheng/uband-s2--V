a8657df0ceb22f784e1699b46130482638817585
hydewww <2208493266@qq.com>
2017-08-09 16:07:45 +0800

    update index

--- run.py
+++ run.py
@@ -5,30 +5,24 @@ import codecs
 
 app = Flask(__name__)
 
-def readJson(student_number):
+def readJson(json_name):
 	file_text=''
 	json_folder = 'static/json/'
-	try:
-		file = codecs.open(json_folder+student_number+'.json', 'r+','utf-8')
-		# file = open(json_folder+student_number+'.json', 'r+',encoding='utf-8')
-	except:
-		print ("!!!cannot open!!!")
-	try:		
-		file_text = file.read()
-		file_text = dict(json.loads(file_text))
-	except:
-		print ("!!!cannot read!!!")
-		print (file_text)
+	file = codecs.open(json_folder+json_name+'.json', 'r+','utf-8')
+	# file = open(json_folder+student_number+'.json', 'r+',encoding='utf-8')
+	file_text = file.read()
+	file_text = json.loads(file_text)
 	return file_text
 
 @app.route('/')
 def homepage():
-	return render_template('index.html')
+	json_text = readJson("index")
+	return render_template('index.html',data=json_text)
 
 @app.route('/<string:student_number>/details')
 def intro(student_number):
 	file_text = readJson(student_number)
-	return render_template(str(student_number)+".html", data=file_text)
+	return render_template(str(student_number)+".html", data=dict(file_text))
 
 if __name__ == '__main__':
 	app.run(debug=True)
