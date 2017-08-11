7937423f61d76ed3bea4ed601e4fdc0707446b25
hydewww <2208493266@qq.com>
2017-08-11 16:47:21 +0800

    update personal page

--- run.py
+++ run.py
@@ -9,7 +9,6 @@ def readJson(json_name):
 	file_text=''
 	json_folder = 'static/json/'
 	file = codecs.open(json_folder+json_name+'.json', 'r+','utf-8')
-	# file = open(json_folder+student_number+'.json', 'r+',encoding='utf-8')
 	file_text = file.read()
 	file_text = json.loads(file_text)
 	return file_text
@@ -21,8 +20,12 @@ def homepage():
 
 @app.route('/<string:student_number>/details')
 def intro(student_number):
-	file_text = readJson(student_number)
-	return render_template(str(student_number)+".html", data=dict(file_text))
+	json_text = readJson("index")
+	student_json ={}
+	for student in json_text:
+		if student["student_number"] == student_number:
+				student_json=student
+	return render_template("person.html", data=student_json)
 
 if __name__ == '__main__':
 	app.run(debug=True)
