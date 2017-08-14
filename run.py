56f55a6debc4ea22a33f9cf72da93a3f5dfb3a1c
SummerCheng <546589068wry@gmial.com>
2017-08-14 00:34:39 -0700

    draft

--- run.py
+++ run.py
@@ -13,6 +13,12 @@ def readJson(json_name):
 	file_text = json.loads(file_text)
 	return file_text
 
+def readData(text):
+	student_json ={}
+	for student in json_text:
+		if student["student_number"] == student_number:
+				return student
+
 @app.route('/')
 def homepage():
 	json_text = readJson("index")
@@ -21,11 +27,27 @@ def homepage():
 @app.route('/<string:student_number>/details')
 def intro(student_number):
 	json_text = readJson("index")
-	student_json ={}
-	for student in json_text:
-		if student["student_number"] == student_number:
-				student_json=student
+	student_json =readData(json_text)
 	return render_template("person.html", data=student_json)
+
+@app.route('/<string:student_number>/academy')
+def acad(student_number):
+	json_text = readJson("index")
+	student_json =readData(json_text)
+	return render_template("academy.html", data=student_json)
+
+@app.route('/<string:student_number>/life')
+def acad(student_number):
+	json_text = readJson("index")
+	student_json =readData(json_text)
+	return render_template("life.html", data=student_json)
+
+@app.route('/<string:student_number>/contact')
+def acad(student_number):
+	json_text = readJson("index")
+	student_json =readData(json_text)
+	return render_template("contact.html", data=student_json)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
